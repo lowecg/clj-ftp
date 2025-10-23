bfc6f06cfaf786c6c9fe9d538e490fe5885c4444
Chris Lowe <chris@scalably.ai>
2025-10-23 06:51:41 +0100

    bump dependencies

--- project.clj
+++ project.clj
@@ -7,11 +7,11 @@
   :deploy-repositories [["releases" :clojars]
                         ["clojars" {:url "https://repo.clojars.org"
                                    :sign-releases false}]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-commons/fs "1.6.311"]
-                 [commons-net "3.11.1"]]
+  :dependencies [[org.clojure/clojure "1.12.3"]
+                 [clj-commons/fs "1.6.312"]
+                 [commons-net "3.12.0"]]
   :profiles {:test {:resource-paths ["test-resources"]
                     :dependencies [[org.mockftpserver/MockFtpServer "3.2.0"]
-                                   [org.slf4j/slf4j-jdk14 "2.0.16"]
+                                   [org.slf4j/slf4j-jdk14 "2.0.17"]
                                    [digest "1.4.10"]]}})
 
